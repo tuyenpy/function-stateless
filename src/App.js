52120a3b1f3cbb51c78519150f2fa602a556b714
songthinhvuong87 <songthinhvuong87@gmail.com>
2020-05-17 16:09:59 +0700

    Refactor use Function stateless

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { Container } from 'react-bootstrap';
 import BookItem from './component/BookItem/BookItem';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      books: [
-        {
-          image: "https://loremflickr.com/320/240",
-          title: "Hatari!",
-          description: "Laceration with foreign body of left back wall of thorax without penetration into thoracic cavity, subsequent encounter"
-        }
-      ]
-    };
-  }
-
-  render() {
-    let { books } = this.state;
+const App = () => {
+    let [ state, setState ] = useState({
+        books: [
+          {
+            image: "https://loremflickr.com/320/240",
+            title: "Hatari!",
+            description: "Laceration with foreign body of left back wall of thorax without penetration into thoracic cavity, subsequent encounter"
+          }
+        ]
+    });
+    let { books } = state;
     return <div className="App">
       <Container>
         {
@@ -29,7 +24,6 @@ class App extends Component {
 
       </Container>
     </div>
-  }
 }
 
 export default App;
--- src/component/BookItem/BookItem.js
+++ src/component/BookItem/BookItem.js
@@ -1,23 +1,19 @@
 import React, { Component } from 'react';
 import { Card, Button } from 'react-bootstrap';
 import PropTypes from 'prop-types';
-class BookItem extends Component {
-    constructor(props) {
-        super();
-    }
-    render() {
-        let { book } = this.props;
-        return <div className="BookItem">
-            <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src={book.image} />
-                <Card.Body>
-                    <Card.Title>{book.title}</Card.Title>
-                    <Card.Text>{book.description}</Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
-                </Card.Body>
-            </Card>
-        </div>
-    }
+
+const BookItem = (props) => {
+    let { book } = props;
+    return <div className="BookItem">
+        <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={book.image} />
+            <Card.Body>
+                <Card.Title>{book.title}</Card.Title>
+                <Card.Text>{book.description}</Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+        </Card>
+    </div>
 }
 
 BookItem.propTypes = {
